80a4232d0931ce06fdb0b35c23d3b8885d68da0b
ahaddad98 <amine.haddadd@gmail.com>
2021-05-06 17:27:29 +0000

    philo_one DONE

--- philo_one/sources/print.c
+++ philo_one/sources/print.c
@@ -6,105 +6,109 @@
 /*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 03:05:22 by amine             #+#    #+#             */
-/*   Updated: 2021/05/06 04:28:25 by amine            ###   ########.fr       */
+/*   Updated: 2021/05/06 17:19:30 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philosopher.h"
 
-void print_action(t_phl *phl, int action)
+void	print_action(t_phl *phl, int action)
 {
-    struct timeval time_print;
-    unsigned int time = 0;
+	struct timeval	time_print;
+	unsigned int	time;
 
-    gettimeofday(&time_print, NULL);
-    time = ((time_print.tv_sec * 1000) + (time_print.tv_usec / 1000)) - ((phl->args->time_to_print.tv_sec * 1000) + (phl->args->time_to_print.tv_usec / 1000));
-    pthread_mutex_lock(&phl->args->print);
-    if (action == 1)
-        printf("%u ==> phl : %d ;  has taken a fork\n", time, phl->num);
-    if (action == 2)
-    {
-        printf("%u ==>phl : %d ;  is eating \n", time, phl->num);
-        phl[0].eating_count[phl->num] = phl[0].eating_count[phl->num] + 1;
-    }
-    if (action == 3)
-        printf("%u ==>phl : %d ; is sleeping \n", time, phl->num);
-    if (action == 4)
-        printf("%u ==>phl : %d ; is thinking\n", time, phl->num);
-    pthread_mutex_unlock(&phl->args->print);
+	time = 0;
+	gettimeofday(&time_print, NULL);
+	time = ((time_print.tv_sec * 1000) + (time_print.tv_usec / 1000))
+		- ((phl->args->time_to_print.tv_sec * 1000)
+			+ (phl->args->time_to_print.tv_usec / 1000));
+	pthread_mutex_lock(&phl->args->print);
+	if (action == 1)
+		printf("%u ==> phl : %d ;  has taken a fork\n", time, phl->num);
+	if (action == 2)
+	{
+		printf("%u ==>phl : %d ;  is eating \n", time, phl->num);
+		phl[0].eating_count[phl->num] = phl[0].eating_count[phl->num] + 1;
+	}
+	if (action == 3)
+		printf("%u ==>phl : %d ; is sleeping \n", time, phl->num);
+	if (action == 4)
+		printf("%u ==>phl : %d ; is thinking\n", time, phl->num);
+	pthread_mutex_unlock(&phl->args->print);
 }
 
-int check_num_must_eat(t_phl *phl)
+int	check_num_must_eat(t_phl *phl)
 {
-    int i;
-    i = 0;
-    while (i < phl->args->number_of_philosopher)
-    {
-        if (phl[0].eating_count[i] < phl->args->time_must_eat)
-            return (0);
-        i++;
-    }
-    return (1);
+	int		i;
+
+	i = 0;
+	while (i < phl->args->number_of_philosopher)
+	{
+		if (phl[0].eating_count[i] < phl->args->time_must_eat)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-void *check_die(void *data)
+void	*check_die(void *data)
 {
-    t_phl *phl;
-    int time_count;
+	t_phl	*phl;
+	int		t_c;
 
-    phl = (t_phl *)data;
-    while (1)
-    {
-        pthread_mutex_lock(&phl->mutex);
-        time_count = 0;
-        gettimeofday(&phl->end_time, NULL);
-        time_count = ((phl->end_time.tv_sec * 1000) + (phl->end_time.tv_usec / 1000)) - ((phl->start_time.tv_sec * 1000) + (phl->start_time.tv_usec / 1000));
-        if (time_count > phl->args->time_to_die)
-        {
-            pthread_mutex_lock(&phl->args->die);
-            pthread_mutex_lock(&phl->args->print);
-            pthread_mutex_unlock(&phl->args->ss);
-            printf("the phl : %d died\n", phl->num);
-        }
-        pthread_mutex_unlock(&phl->mutex);
-        usleep(1000);
-    }
-    return (NULL);
+	phl = (t_phl *)data;
+	while (1)
+	{
+		pthread_mutex_lock(&phl->mutex);
+		t_c = 0;
+		gettimeofday(&phl->end_time, NULL);
+		t_c = ((phl->end_time.tv_sec * 1000) + (phl->end_time.tv_usec / 1000))
+			- ((phl->start_time.tv_sec * 1000)
+				+ (phl->start_time.tv_usec / 1000));
+		if (t_c > phl->args->time_to_die)
+		{
+			pthread_mutex_lock(&phl->args->die);
+			pthread_mutex_lock(&phl->args->print);
+			pthread_mutex_unlock(&phl->args->ss);
+			printf("the phl : %d died\n", phl->num);
+		}
+		pthread_mutex_unlock(&phl->mutex);
+		usleep(1000);
+	}
+	return (NULL);
 }
 
-void *amine(char *msg, t_phl *phl)
+void	*amine(char *msg, t_phl *phl)
 {
-    pthread_mutex_lock(&phl->args->die);
-    printf("DONE\n");
-    // pthread_mutex_lock(&phl->args->print);
-    pthread_mutex_unlock(&phl->args->ss);
-    return (NULL);
-}     
+	pthread_mutex_lock(&phl->args->die);
+	printf("DONE\n");
+	pthread_mutex_unlock(&phl->args->ss);
+	return (NULL);
+}
 
-void *action(void *data)
+void	*action(void *data)
 {
-    t_phl *phl;
-    int k;
-    pthread_mutex_t test;
+	t_phl				*phl;
+	int					k;
+	pthread_mutex_t		test;
 
-    phl = (t_phl *)data;
-    int i = 0;
-    gettimeofday(&phl->start_time, NULL);
-    pthread_mutex_init(&test, NULL);
-    pthread_create(&phl->thrd, NULL, &check_die, phl);
-    pthread_detach(phl->thrd);
-    while (1)
-    {
-        if (phl->args->time_must_eat != -1)
-        {
-            k = get_fork(phl);
-            if (k == 2)
-                break;
-            if (k == 1)
-                return (amine(NULL, phl));
-        }
-        else
-            get_fork(phl);
-    }
-    return (NULL);
+	phl = (t_phl *)data;
+	gettimeofday(&phl->start_time, NULL);
+	pthread_mutex_init(&test, NULL);
+	pthread_create(&phl->thrd, NULL, &check_die, phl);
+	pthread_detach(phl->thrd);
+	while (1)
+	{
+		if (phl->args->time_must_eat != -1)
+		{
+			k = get_fork(phl);
+			if (k == 2)
+				break ;
+			if (k == 1)
+				return (amine(NULL, phl));
+		}
+		else
+			get_fork(phl);
+	}
+	return (NULL);
 }
--- philo_one/sources/tools3.c
+++ philo_one/sources/tools3.c
@@ -6,12 +6,11 @@
 /*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 16:04:41 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/05/04 03:07:17 by amine            ###   ########.fr       */
+/*   Updated: 2021/05/06 17:23:27 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philosopher.h"
-
 
 int	ft_isdigit(int c)
 {
@@ -20,39 +19,41 @@ int	ft_isdigit(int c)
 	return (0);
 }
 
-int is_number(char *str)
+int	is_number(char *str)
 {
-    int i = 0;
+	int		i;
 
-    while (str[i])
-    {
-        if (ft_isdigit(str[i]) == 0)
-            return (1);
-        i++;
-    }
-    return (0); 
+	i = 0;
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]) == 0)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-int check_args(char **av, int ac)
+int	check_args(char **av, int ac)
 {
-    int i = 1;
+	int		i;
 
-    while (av[i])
-    {
-        if (is_number(av[i]))
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 1;
+	while (av[i])
+	{
+		if (is_number(av[i]))
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-void get_args(char **av, t_args *args)
+void	get_args(char **av, t_args *args)
 {
-    args->time_must_eat = -1;
-    args->number_of_philosopher = ft_atoi(av[1]);
-    args->time_to_die = ft_atoi(av[2]);
-    args->time_to_eat = ft_atoi(av[3]);
-    args->time_to_sleep = ft_atoi(av[4]);
-    if (av[5])
-        args->time_must_eat = ft_atoi(av[5]);
+	args->time_must_eat = -1;
+	args->number_of_philosopher = ft_atoi(av[1]);
+	args->time_to_die = ft_atoi(av[2]);
+	args->time_to_eat = ft_atoi(av[3]);
+	args->time_to_sleep = ft_atoi(av[4]);
+	if (av[5])
+		args->time_must_eat = ft_atoi(av[5]);
 }
